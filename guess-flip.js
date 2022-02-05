6a3a74e1ed56fc64a02d445155b3a2251b027f31
max-muoto <41130755+max-muoto@users.noreply.github.com>
2022-02-05 15:24:36 -0500

    added flipACoin call

--- guess-flip.js
+++ guess-flip.js
@@ -1,4 +1,5 @@
 import minimist from 'minimist'
+import {flipACoin} from './modules/coin.mjs';
 
 const argv = (minimist)(process.argv.slice(2));
 
@@ -9,5 +10,5 @@ const cll = argv.call;
 if (cll === undefined) {
     console.log("Error: there was no input")
 } else {
-    
+    console.log(flipACoin(cll));
 }
