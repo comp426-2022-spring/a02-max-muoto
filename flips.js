0924f2ec3fc14e6324f65a6417da3e087df3c642
max-muoto <41130755+max-muoto@users.noreply.github.com>
2022-02-13 15:59:05 -0500

    Additional Fixes

--- flips.js
+++ flips.js
@@ -1,9 +1,12 @@
 import minimist from 'minimist'
-import {coinFlips} from './modules/coin.mjs';
+import {coinFlips, countFlips} from './modules/coin.mjs';
 
 const argv = (minimist)(process.argv.slice(2));
 
 argv["number"];
 const amt = argv.number || 1;
 
-console.log(coinFlips(amt));
+let cFlips = (coinFlips(amt));
+
+console.log(cFlips);
+console.log(countFlips(cFlips));
